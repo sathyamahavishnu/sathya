5933826b598a11df444713c241a23245d1e12707
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-11 15:34:33 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+x=int(intput("enter the Alphabets"))
+v=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+for y in v:
+     if(y==x):
+         print("Alphabet")
+     else :
+         print("no")
