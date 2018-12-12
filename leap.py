85f5b6d29b8f2c1181d3f69534b92726d8782329
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-12 12:07:05 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -2,8 +2,8 @@ year = int(input(""))
 if(year % 4 == 0 and year % 100 != 0):
     print("Leap Year")
 elif(year % 100 == 0):
-    print("not Leap Year")
+    print("no")
 elif(year % 400 ==0):
     print("Leap Year")
 else:
-    print("not Leap Year")
+    print("no")
