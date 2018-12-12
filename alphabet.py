41fb971189f282b3abf21e740d88935a5aa97d8c
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-12 10:55:42 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,6 @@
-h=input("")
-if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-        print("Alphabet")
+ch=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','u','v','w','x','y','z',)
+n=(1,2,3,4,5,6,7,8,9)
+if(ch!=n):
+        print("alphabet")
 else:
-        print(" No")
+        print(" no")
