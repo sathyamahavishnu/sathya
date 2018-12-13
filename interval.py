c3600a2e2013e7ae9dfbdf6ea3234794afb9ac99
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-13 14:54:23 +0530

    Update interval.py

--- interval.py
+++ interval.py
@@ -1,5 +1,5 @@
-lower=int(input("enter lower limit"))
-upper=int(input("enter upper limit"))
+lower=int(input(""))
+upper=int(input(""))
 for i in range(lower,upper+1):
     if(i%2!=0):
         print(i)
