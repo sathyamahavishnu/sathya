3e8014d345def3ba4f45ea8204b72f6ab501e643
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-13 12:12:45 +0530

    Update power.py

--- power.py
+++ power.py
@@ -3,6 +3,6 @@ import math
 number = int(input(""))
 exponent = int(input(""))
 
-power = math.pow(number, exponent)
+power = math.pow(number exponent)
     
 print("{0} Power {1} = {2}".format(number, exponent, power))
