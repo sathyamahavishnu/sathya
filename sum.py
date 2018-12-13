cf00a6f2b9f6188333d837f597328bd1c50cfa32
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-13 09:49:42 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,11 +1,11 @@
 num = int(input(""))
-if num == 0:
+if num==0:
     exit()
-elif num < 1:
+elif num<1:
     print("")
 else:
     sum = 0
-    while num > 0:
-    	sum += num
-    	num -= 1
+    while num>0:
+    	sum+=num
+    	num-=1
     print("Sum")
