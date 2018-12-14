ef743c24899cda0c767910195945e9959be7a5ab
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-14 14:25:04 +0530

    Update prime interval.py

--- prime interval.py
+++ prime interval.py
@@ -1,11 +1,10 @@
 lower=int(input(""))
 upper=int(input(""))
-
 for num in range(lower,upper+1):
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
                break
-       else:
-           print(num)
+           else:
+             print(num)
     
