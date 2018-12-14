987a135a798b19801817002a50238d0f2fe3be10
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-14 14:58:40 +0530

    Update primeinterval.py

--- primeinterval.py
+++ primeinterval.py
@@ -3,8 +3,8 @@ upper=int(input(""))
 for num in range(lower,upper+1):
    if num>1:
        for i in range(2,num):
-           if (num % i)==0:
-               break
+           if(num % i)==0:
+              break
            else:
              print(num)
     
