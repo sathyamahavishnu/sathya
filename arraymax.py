e22eff16ceb98033a5c2f3ca91bbca0f504eb8df
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-17 14:43:38 +0530

    Update arraymax.py

--- arraymax.py
+++ arraymax.py
@@ -1,10 +1,10 @@
 def largest(arr,n):  
-    max=arr[0]  
-    for i in range(1, n): 
-        if arr[i]>max: 
+   max=arr[0]  
+   for i in range(1, n): 
+       if arr[i]>max: 
            ma=arr[i] 
-    return max 
-arr=[1 ,2,3,4,5] 
+    eturn max 
+arr=[1,2,3,4,5] 
 n=len(arr) 
 lar=largest(arr,n) 
 print(lar)
