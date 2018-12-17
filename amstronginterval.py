f7f4c0be8b815d9ee86ec509dffc4bebaf01fcee
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-17 11:37:59 +0530

    Update amstronginterval.py

--- amstronginterval.py
+++ amstronginterval.py
@@ -2,8 +2,8 @@ for num in range(1,100):
   temp=num
   sum=0
   while temp>0:
-      digit=temp%10
-      sum=sum+digit**3
-      temp=temp//10
-      if sum==num:
-           print (num)
+     digit=temp%10
+     sum=sum+digit**3
+     temp=temp//10
+     if sum==num:
+       print (num)
