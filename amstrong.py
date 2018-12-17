7236321cb8c83e63ec72715550522032ad219ff4
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-17 10:33:41 +0530

    Create amstrong.py

---
+++ amstrong.py
@@ -0,0 +1,12 @@
+no=int(input(""))
+temp=no
+sum=0
+while no>0:
+    rem=no%10
+    sum=sum+rem*rem*rem
+    no=no//10
+if sum==temp:
+    print("amstrong number")
+else:
+    print(" not amstrong number"
+)
