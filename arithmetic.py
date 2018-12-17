1b7e0c998f47e3e3e251ebd20aeaa72a4b83d353
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-17 14:06:13 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,11 +1,11 @@
 def sumOfAP( a, d,n): 
-     sum=0
-     i=0 
-     while i<n : 
+   sum=0
+   i=0 
+   while i<n : 
          sum = sum+a 
          a=a+d 
          i=i+1
-     return sum
+   return sum
 n=int(input(""))
 a=int(input(""))
 d=int(input(""))
