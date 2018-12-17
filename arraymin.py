5103e49427491dfa5a0219ac015f13b8c84e7143
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-17 15:08:56 +0530

    Create arraymin.py

---
+++ arraymin.py
@@ -0,0 +1,10 @@
+def largest(arr,n):  
+    min=arr[0]  
+    for i in range(1, n): 
+        if arr[i]<min: 
+            mi=arr[i] 
+    return min 
+arr=[1 2 3] 
+n=len(arr) 
+lar= largest(arr,n) 
+print (lar) 
