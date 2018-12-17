f7bb5ec99e0c39bf191770a13cf513bd99f46805
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-17 12:17:33 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,12 +1,12 @@
 def factorial(num):
     if num == 1:
-        return num
+       return num
     else:
-        return num * factorial(num - 1)
+       return num * factorial(num - 1)
 num=int(input(""))
 if num<0:
-    print("Factorial  negative number")
+   print("Factorial  negative number")
 elif num==0:
-    print("Factorial of 0 is 1")
+   print("Factorial of 0 is 1")
 else:
-    print(factorial(num))
+   print(factorial(num))
