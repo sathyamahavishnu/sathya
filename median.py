b55bfffc7bd8c00ce5f1041fe1df16cd58df331e
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-18 10:48:41 +0530

    Update median.py

--- median.py
+++ median.py
@@ -1,8 +1,8 @@
-nums = [1,2,3,4,5]
+nums=[1,2,3,4,5]
 nums.sort()
-length = len(nums)
+length=len(nums)
 if (length % 2 == 0):
-    median = (nums[(length)//2] + nums[(length)//2-1]) / 2
+     median=(nums[(length)//2] + nums[(length)//2-1]) / 2
 else:
-    median = nums[(length-1)//2]
+     median=nums[(length-1)//2]
 print(median)
