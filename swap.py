315fffe65263b49a842d2efef9542f9d5cf97cd3
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-21 15:18:53 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,9 +1,9 @@
-num1=int(input(""))
-num2=int(input(""))
+num1 = int(input(""))
+num2 = int(input(""))
 print(num1)
 print(num2)
-temp=num1
-num1=num2
-num2=temp
+temp = num1
+num1 = num2
+num2 = temp
 print(num1)
 print(num2)
