4c2ad3033bb253d0f812fa515406640390445bd9
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-22 11:15:44 +0530

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,16 @@
+str1 = raw_input("")
+str2 = raw_input("")
+count1 = 0
+count2 = 0
+for i in str1:
+      count1=count1+1
+for j in str2:
+      count2=count2+1
+if(count1<count2):
+      print("")
+      print(str2)
+elif(count1==count2):
+      print("Both strings are equal.")
+else:
+      print("Larger string is:")
+      print(str1)
