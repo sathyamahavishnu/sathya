c4fd6b193093b4ac46f1cb084777b14611623656
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-22 10:18:16 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,10 @@
 def recur_fibo(n):
-   if n <= 1:
+   if n<=1:
        return n
    else:
        return(recur_fibo(n-1) + recur_fibo(n-2))
-nterms = 10
-if nterms <= 0:
+nterms=10
+if nterms<=0:
    print("positive integer")
 else:
    print("")
