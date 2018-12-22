aa6e16a5ac3eddc86a008ae21b600408ff5b24cc
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-22 09:23:47 +0530

    Update bitwise.py

--- bitwise.py
+++ bitwise.py
@@ -1,8 +1,8 @@
 
-num1=int(input(""))
-num2=int(input(""))
-num1=num1^num2
-num2=num1^num2
-num1=num1^num2
+num1 = int(input(""))
+num2 = int(input(""))
+num1 = num1^num2
+num2 = num1^num2
+num1 = num1^num2
 print(num1)
 print(num2)
