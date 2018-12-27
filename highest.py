67ef714d8880b8bfad9b51dd477871deca78a5c5
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-27 22:08:37 +0530

    Update highest.py

--- highest.py
+++ highest.py
@@ -3,4 +3,5 @@ num = int(input("How many numbers:"))
 for n in range(num):
     numbers = int(input("number")
     lst.append(numbers)
-print("highest number:", high(lst), "\nlowest number:", low(lst)) 
+print(high(lst))
+print(low(lst)) 
