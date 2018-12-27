8691c9055071df64cd5dc7b396cf2babb8543f98
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2018-12-27 22:30:48 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,7 +1,7 @@
-num = int(input("How many numbers:"))
-total_sum = 0
+num=int(input("How many numbers:"))
+total_sum=0
 for n in range(num):
-    numbers = float(input("numbers:"))
-    total_sum += numbers
-avg = total_sum/num
+     numbers=float(input("numbers:"))
+     total_sum +=numbers
+avg=total_sum/num
 print(avg)
