73ba0c8679f2dc16db275228ecf582019faaeab5
sathyamahavishnu <45752268+sathyamahavishnu@users.noreply.github.com>
2019-02-24 22:14:54 +0530

    Update highest.py

--- highest.py
+++ highest.py
@@ -1,7 +1,7 @@
 a=[]
 num=int(input("How many numbers:"))
 for n in range(num):
-    numbers=int(input("number")
+    numbers=int(input("number"))
     a.append(numbers)
-print(high(a))
-print(low(a)) 
+print(max(a))
+print(min(a)) 
